4875df7521b99a8114c9bbbac421a68f28168878
atad <1585232641@qq.com>
2022-06-10 16:27:28 +0800

    修改shell脚本

--- setdocker.sh
+++ setdocker.sh
@@ -2,6 +2,7 @@
 
 date
 
+cd go
 rm -rf new
 mkdir new && cd new
 
@@ -12,12 +13,35 @@ cd StuManager
 echo "Please input docker container ID: "
 read x
 docker stop $x
-docke rm $x
+docker rm $x
+
+echo "Please input docker images ID: "
+read ys
+docker rmi $y
+echo "Please input image tag: "
+read z
+docker build -t stumanager:$z .
+docker run --name stumanager -p 8000:8000 -d stumanager:$z
+
+cd ..
+cd ..
+cd react 
+rm -rf new
+mkdir new && cd new
+
+git clone https://ghproxy.com/https://github.com/littlepi-bit/StuManagerFront.git
+cd StuManagerFront
+
+echo "Please input docker container ID: "
+read x
+docker stop $x
+docker rm $x
 
 echo "Please input docker images ID: "
 read y
 docker rmi $y
 echo "Please input image tag: "
 read z
-docker build -t stumanager:$z .
-docker run --name stumanager -p 8000:8000 -d stumanager:$z
+docker build -t stumanagerfront:$z .
+docker run --name stumanagerfront -p 3000:3000 -d stumanagerfront:$z
+docker ps -a
